1f9bd49a06bc05b044d518973c2e30476d39c2e9
Supatsorn Poonsawat <88094850+supstsorn@users.noreply.github.com>
2023-10-12 21:37:17 +0700

    Update Program.cs

--- demoNine/Program.cs
+++ demoNine/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq.Expressions;
+namespace demoNine;
+class Program
+{
+    static string ReverseWords (string str)
+    {
+        string a = "";
+        foreach (char item in str)
+        {
+            a = item + a;
+        }
+        return a;
+    }
+    static void Main(string[] args){
+        string reverse = ReverseWords("Welcome to clicknext");
+        Console.WriteLine(reverse);
+    }
+
+}
